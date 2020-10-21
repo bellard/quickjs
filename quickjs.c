a37183d93e91c1adf06a31e403b68f4013c576e2
Andrew <andrew.fedoniouk@gmail.com>
2020-10-20 20:23:06 -0700

    fixes issue in var [a,b,c,d] = ... destructuring assignment

--- quickjs.c
+++ quickjs.c
@@ -24115,6 +24115,8 @@ static int js_parse_destructuring_element(JSParseState *s, int tok, int is_arg,
                         goto var_error;
                     opcode = OP_scope_get_var;
                     scope = s->cur_func->scope_level;
+                    label_lvalue = -1;
+                    depth_lvalue = 0;
                 } else {
                     if (js_parse_postfix_expr(s, TRUE))
                         return -1;
