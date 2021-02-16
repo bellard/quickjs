c3181c46fe0ec105907d585f804ff6a19858566b
Yonggang Luo <luoyonggang@gmail.com>
2021-02-16 10:00:31 +0000

    msvc doesn't support for __int128

--- libbf.h
+++ libbf.h
@@ -27,7 +27,7 @@
 #include <stddef.h>
 #include <stdint.h>
 
-#if INTPTR_MAX >= INT64_MAX
+#if INTPTR_MAX >= INT64_MAX && !defined(_MSC_VER)
 #define LIMB_LOG2_BITS 6
 #else
 #define LIMB_LOG2_BITS 5
