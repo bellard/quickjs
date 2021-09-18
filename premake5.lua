2af70e45e909f43a18096727504c289061792646
local <local>
2021-09-18 20:02:28 +0800

    fix: compile error on Windows

--- premake5.lua
+++ premake5.lua
@@ -74,7 +74,7 @@ workspace "quickjs"
 		systemversion "latest"
 
 	filter { }
-		targetdir ".bin/%{cfg.longname}/"
+		targetdir ".bin/%{cfg.buildcfg}/"
 		exceptionhandling "Off"
 		rtti "Off"
 		--vectorextensions "AVX2"
