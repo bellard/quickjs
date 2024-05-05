d9c699f528755360e32f7d2755ad3f91b575b4f9
Richard Davison <richard.davison1@gmail.com>
2024-05-05 18:46:30 +0200

    fix class method with name get (#258)
    Co-authored-by: Richard Davison <ridaviso@amazon.com>

--- quickjs.c
+++ quickjs.c
@@ -22380,7 +22380,8 @@ static int __exception js_parse_property_name(JSParseState *s,
             if (next_token(s))
                 goto fail1;
             if (s->token.val == ':' || s->token.val == ',' ||
-                s->token.val == '}' || s->token.val == '(') {
+                s->token.val == '}' || s->token.val == '(' ||
+                s->token.val == '=' ) {
                 is_non_reserved_ident = TRUE;
                 goto ident_found;
             }
