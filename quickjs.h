6f9d05fd2b9de2f132b22518dabf201cea1c4fba
Kasper Isager Dalsgarð <kasperisager@hey.com>
2024-05-09 12:45:47 +0200

    Expose `JS_SetUncatchableError()` (#262)
    * Expose `JS_SetUncatchableError()`
    
    * Remove unnecessary `JS_SetUncatchableError` declaration

--- quickjs.h
+++ quickjs.h
@@ -635,6 +635,7 @@ JSValue JS_Throw(JSContext *ctx, JSValue obj);
 JSValue JS_GetException(JSContext *ctx);
 JS_BOOL JS_HasException(JSContext *ctx);
 JS_BOOL JS_IsError(JSContext *ctx, JSValueConst val);
+void JS_SetUncatchableError(JSContext *ctx, JSValueConst val, JS_BOOL flag);
 void JS_ResetUncatchableError(JSContext *ctx);
 JSValue JS_NewError(JSContext *ctx);
 JSValue __js_printf_like(2, 3) JS_ThrowSyntaxError(JSContext *ctx, const char *fmt, ...);
