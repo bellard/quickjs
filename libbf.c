f4d9fcfc5b183a4183d74ccd1ba1a3557d546993
3393304070 <49681813+3393304070@users.noreply.github.com>
2024-09-15 17:59:56 +0100

    Fixed out-of-bound write in libbf.c

--- libbf.c
+++ libbf.c
@@ -1715,6 +1715,11 @@ static int __bf_div(bf_t *r, const bf_t *a, const bf_t *b, limb_t prec,
         slimb_t d;
 
         na = n + nb;
+      
+        if (na >= (SIZE_MAX / sizeof(limb_t)) - 1) {
+            return BF_ST_MEM_ERROR;
+        }
+      
         taba = bf_malloc(s, (na + 1) * sizeof(limb_t));
         if (!taba)
             goto fail;
